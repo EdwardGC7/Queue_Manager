20bfb1c3ee93ad6e360d6055f3dd754319213ffd
AlexSalva <alex_salvador20@hotmail.com>
2024-10-12 08:39:45 -0400

    cree la lsita de cartas en la pantalla de mis citas

--- lib/features/authentication/screens/citas/mis_citas.dart
+++ lib/features/authentication/screens/citas/mis_citas.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:queue_manager/utils/constants/colors.dart';
 
 class MisCitas extends StatefulWidget {
   const MisCitas({super.key});
@@ -12,7 +13,32 @@ class _MisCitasState extends State<MisCitas> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Citas"),
+        automaticallyImplyLeading: false,
+        title: const Text(
+          "Citas",
+          style: TextStyle(color: GColors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: GColors.primaryGreen,
+      ),
+      body: ListView.builder(
+        shrinkWrap: true,
+        itemCount: 20,
+        itemBuilder: (_, index) {
+          return const Card(
+            shadowColor: GColors.primaryGreen,
+            elevation: 100,
+            child: ListTile(
+              title: Text(
+                "Jon Barbershop",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text("nota"),
+              trailing: Text("09:30 AM"),
+              leading: Text("4"),
+            ),
+          );
+        },
       ),
     );
   }
